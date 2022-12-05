fbe0b58bb2adc3bdca6c6ebf83b73e60e454e7a7
paul.mandl <paul.mandl@findustrial.io>
2022-12-05 14:40:04 +0100

    dd

--- benchmark/polygon/env.sh
+++ benchmark/polygon/env.sh
@@ -1,12 +1,12 @@
 # folder that contains all benchmark scripts (this could be on a network share)
-ETH_HOME=/home/ubuntu/blockbench/benchmark/ethereum
+ETH_HOME=/home/ubuntu/blockbench/benchmark/polygon
 # file that contains ip addresses of servers that should be used for setting up the ethereum network
-HOSTS=/Users/paulmandl/Downloads/blockbench-master-mehd/benchmark/ethereum/hosts
+HOSTS=/Users/paulmandl/Downloads/blockbench-master-mehd/benchmark/polygon/hosts
 # where is your hostfile on your client vm?
-CLIENT_HOSTS=/home/ubuntu/blockbench/benchmark/ethereum/hosts_priv
+CLIENT_HOSTS=/home/ubuntu/blockbench/benchmark/polygon/hosts_priv
 RPC_PORT=8051
 # file that contains ip addresses of servers that should be used for running the benchmark clients
-CLIENTS=/Users/paulmandl/Downloads/blockbench-master-mehd/benchmark/ethereum/clients
+CLIENTS=/Users/paulmandl/Downloads/blockbench-master-mehd/benchmark/polygon/clients
 # folder in which ethereum nodes should store the ethereum data
 ETH_DATA=/home/ubuntu/node1
 # folder in which benchmark clients should store their log files

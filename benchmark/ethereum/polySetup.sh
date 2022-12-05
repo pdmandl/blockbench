b11fee09e98915861db704d29ee137a59ab4e1bf
paul.mandl <paul.mandl@findustrial.io>
2022-12-05 12:44:28 +0100

    poly refactor

--- benchmark/ethereum/polySetup.sh
+++ benchmark/ethereum/polySetup.sh
@@ -1,3 +1,5 @@
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.19.3.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
 go install
 git clone https://github.com/0xPolygon/polygon-edge.git
 cd polygon-edge/

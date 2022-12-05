97e81a96f90ad8eb4f1389cccb5b453bc0e1f321
paul.mandl <paul.mandl@findustrial.io>
2022-12-05 13:45:58 +0100

    ee

--- benchmark/ethereum/initPoly.sh
+++ benchmark/ethereum/initPoly.sh
@@ -3,15 +3,5 @@ cd `dirname ${BASH_SOURCE-$0}`
 . env.sh
 
 polygon-edge secrets init --data-dir $ETH_DATA > secrets.txt
-i=0
-pubKey=""
-blsPubKey=""
-nodeId=""
-cat secrets.txt | while read line 
-do
-    let i=$i+1
-    if [[ $i -eq 3]]; then
-      echo $i ':' $line
-      echo TIME
-    fi        
-done
+python3 $ETH_HOME/prepareSecrets.py
+

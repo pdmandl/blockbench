3905f79f0e32e85882e04cf36915b8f50986fe5d
paul.mandl <paul.mandl@findustrial.io>
2022-12-05 13:52:21 +0100

    prepare secrets

--- benchmark/ethereum/prepareSecrets.py
+++ benchmark/ethereum/prepareSecrets.py
@@ -1,6 +1,8 @@
+import socket
+hostname = socket.gethostname()
+IPAddr = socket.gethostbyname(hostname)
+
 file1 = open('secrets.txt', 'r')
 Lines = file1.readlines()
-
-print(Lines[2])
-print(Lines[3])
-print(Lines[4])
+node_id = Lines[4].split("=")[1]
+print("/ip4/" + IPAddr + "/tcp/1478/p2p/" + node_id)

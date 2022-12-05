5d08772bd8bb649bbb1d9cb59211ee955f30e27d
paul.mandl <paul.mandl@findustrial.io>
2022-12-05 13:16:04 +0100

    poly all script

--- benchmark/ethereum/initPoly.sh
+++ benchmark/ethereum/initPoly.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-# num_nodes
+#args: number_of_nodes
 cd `dirname ${BASH_SOURCE-$0}`
 . env.sh
 
-i=0
-for host in `cat $HOSTS`; do
-  if [[ $i -lt $1 ]]; then
-    ssh -oStrictHostKeyChecking=no $USER@$host polygon-edge secrets init --data-dir $ETH_DATA
-    echo done node $host
-  fi
-  let i=$i+1
-done
+polygon-edge secrets init --data-dir $ETH_DATA > secrets.txt
+
+cat secrets.txt | while read line 
+    echo $line

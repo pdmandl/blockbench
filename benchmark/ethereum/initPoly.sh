5aa54f6746f7ad87a6db83684eb60e8cdf1dfa33
paul.mandl <paul.mandl@findustrial.io>
2022-12-05 13:57:23 +0100

    cc

--- benchmark/ethereum/initPoly.sh
+++ benchmark/ethereum/initPoly.sh
@@ -3,5 +3,6 @@ cd `dirname ${BASH_SOURCE-$0}`
 . env.sh
 
 polygon-edge secrets init --data-dir $ETH_DATA > secrets.txt
-python3 $ETH_HOME/prepareSecrets.py
+IP_ADDR_EXT=`(ip -4 addr | grep -oP '(?<=inet\s)\d+(\.\d+){3}') | cut -f2 -d$'\n'`
+python3 $ETH_HOME/prepareSecrets.py $IP_ADDR_EXT
 

dd0d5a6d85d2e2852b91bd4ede349f17b35a00af
paul.mandl <paul.mandl@findustrial.io>
2022-12-07 23:02:18 +0100

    m

--- benchmark/zkSync/zk_local/start.sh
+++ benchmark/zkSync/zk_local/start.sh
@@ -3,7 +3,9 @@ sudo apt install -y apt-transport-https ca-certificates curl software-properties
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo yadd-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 sudo apt install -y docker-ce
+sudo groupadd docker
 sudo usermod -aG docker ubuntu
+newgrp docker
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 

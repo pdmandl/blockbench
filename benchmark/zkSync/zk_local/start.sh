863b7dd93789e377c4e73a0aa426579746624d83
paul.mandl <paul.mandl@findustrial.io>
2022-12-07 22:29:34 +0100

    zksync docker setup

--- benchmark/zkSync/zk_local/start.sh
+++ benchmark/zkSync/zk_local/start.sh
@@ -4,7 +4,7 @@ sudo chmod +x /usr/local/bin/docker-compose
 
 cd /home/ubuntu/blockbench/benchmark/zkSync/zk_local
 mkdir -p ./volumes
-mkdir -p ./volumes/postgres ./volumes/zksync/env/dev ./volumes/zksync/data
+mkdir -p ./volumes/postgres ./volumes/geth ./volumes/zksync/env/dev ./volumes/zksync/data
 
 docker-compose up
 

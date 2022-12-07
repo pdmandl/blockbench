dc6db38a34bb1f4f01653817de7a832d2a609676
paul.mandl <paul.mandl@findustrial.io>
2022-12-07 23:47:07 +0100

    zk install sh

--- benchmark/zkSync/install.sh
+++ benchmark/zkSync/install.sh
@@ -13,10 +13,9 @@ sudo apt install -y apt-transport-https ca-certificates curl software-properties
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 sudo apt install -y docker-ce
-#sudo groupadd docker
+sudo groupadd docker
 sudo usermod -aG docker ubuntu
 newgrp docker
-reboot
 #sudo apt-get install docker-engine
 #sudo service docker start
 

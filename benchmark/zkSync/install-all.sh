5fb0e928b2251c0d1c716f1e140cc8f1ae118969
paul.mandl <paul.mandl@findustrial.io>
2022-12-07 23:17:27 +0100

    push

---
+++ benchmark/zkSync/install-all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# num_nodes
+cd `dirname ${BASH_SOURCE-$0}`
+. env.sh
+
+i=0
+for host in `cat $HOSTS`; do
+  if [[ $i -lt $1 ]]; then
+    #$1 is the number of nodes
+    #$2 is the private key file to import for use in geth
+    ssh -oStrictHostKeyChecking=no $USER@$host chmod 755 $ETH_HOME/install.sh 
+    ssh -oStrictHostKeyChecking=no $USER@$host $ETH_HOME/install.sh
+    echo done node $host
+  fi
+  let i=$i+1
+done
--- benchmark/zkSync/install.sh
+++ benchmark/zkSync/install.sh
@@ -8,6 +8,15 @@ sudo apt-get install apt-transport-https ca-certificates
 #echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" | sudo tee /etc/apt/sources.list.d/docker.list
 sudo apt-get update
 sudo apt-get install -y ethereum
+#!/usr/bin/env bash
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt install -y docker-ce
+sudo groupadd docker
+sudo usermod -aG docker ubuntu
+newgrp docker
+reboot
 #sudo apt-get install docker-engine
 #sudo service docker start
 

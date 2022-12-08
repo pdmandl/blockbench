9457574230137260ac7d4180e8fafe0c9d6cce1b
paul.mandl <paul.mandl@findustrial.io>
2022-12-08 13:39:35 +0100

    b

--- benchmark/ethereum/start-mining.sh
+++ benchmark/ethereum/start-mining.sh
@@ -3,7 +3,7 @@ cd `dirname ${BASH_SOURCE-$0}`
 . env.sh
 
 IP_ADDR_EXT=`(ip -4 addr | grep -oP '(?<=inet\s)\d+(\.\d+){3}') | cut -f2 -d$'\n'`
-echo $(cat $ETH_HOME/bootnode.txt | tr -d '"')
+sudo ntpdate -s time.nist.gov
 nohup geth --datadir $ETH_DATA --bootnodes $(cat $ETH_HOME/bootnode.txt | tr -d '"') --nodiscover --http --http.addr $IP_ADDR_EXT  --http.port 8051 --http.corsdomain "*" --rpc.gascap 0 --maxpeers 32 --networkid 1 --nat extip:$IP_ADDR_EXT --unlock 0 --allow-insecure-unlock --password <(echo -n "${PWD}") --mine --miner.threads 2 > $ETH_DATA/../eth_log 2>&1 &
 # 
 sleep 2

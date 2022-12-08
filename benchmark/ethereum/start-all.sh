2b1223616b960f8b73258f5ccc3a3723d22f816f
paul.mandl <paul.mandl@findustrial.io>
2022-12-08 12:08:02 +0100

    refactor bootnodes

--- benchmark/ethereum/start-all.sh
+++ benchmark/ethereum/start-all.sh
@@ -7,14 +7,15 @@ rm -rf addPeer.txt
 rm -rf bootnode.txt
 ./gather.sh $1
 sleep 3
-PEERS=`cat $ETH_HOME_LOCAL/addPeer.txt`
-BOOTNODE=`cat $ETH_HOME_LOCAL/bootnode.txt`
-i=0
 for host in `cat $HOSTS`; do
   if [[ $i -lt $1 ]]; then
     echo start mining on node $host
+    scp -oStrictHostKeyChecking=no $ETH_HOME_LOCAL/addPeer.txt $USER@$host:$ETH_HOME
+    scp -oStrictHostKeyChecking=no $ETH_HOME_LOCAL/bootnode.txt $USER@$host:$ETH_HOME
+    ssh -oStrictHostKeyChecking=no $USER@$host chmod 755 $ETH_HOME/addPeer.txt
+    ssh -oStrictHostKeyChecking=no $USER@$host chmod 755 $ETH_HOME/bootnode.txt
     ssh -oStrictHostKeyChecking=no $USER@$host chmod 755 $ETH_HOME/start-mining.sh
-    ssh -oStrictHostKeyChecking=no $USER@$host $ETH_HOME/start-mining.sh ($PEERS | tr -d '\n') ($BOOTNODE | tr -d '\n')
+    ssh -oStrictHostKeyChecking=no $USER@$host $ETH_HOME/start-mining.sh $PEERS $BOOTNODE
     echo done node $host
   fi
   let i=$i+1

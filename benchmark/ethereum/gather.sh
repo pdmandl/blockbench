29e2628cbf92cfbe07f3fa3d22282bf753054639
paul.mandl <paul.mandl@findustrial.io>
2022-12-08 18:08:50 +0100

    vv

--- benchmark/ethereum/gather.sh
+++ benchmark/ethereum/gather.sh
@@ -7,9 +7,9 @@ i=0
 for host in `cat $HOSTS`; do
   if [[ $i -lt 1 ]]; then
     ssh $USER@$host chmod 755 $ETH_HOME/bootnode_startup.sh
-    echo -n `ssh $USER@$host $ETH_HOME/bootnode_startup.sh >> bootnode.txt
+    echo -n `ssh $USER@$host $ETH_HOME/bootnode_startup.sh` >> bootnode.txt
   fi
-  if [[ $i -lt $1 ]]; then
+  if [[ $i -lt $1 ]]; thenv
     echo "adding peer to network, rpcport $RPCPORT"
     ssh $USER@$host chmod 755 $ETH_HOME/enode.sh
     echo "'admin.addPeer(` ssh $USER@$host $ETH_HOME/enode.sh $host 2>/dev/null | grep enode`)'" >> addPeer.txt

b37bb1e089905b6cbdb910827d53f59c192c63f2
paul.mandl <paul.mandl@findustrial.io>
2022-12-08 18:10:37 +0100

    bbb

--- benchmark/ethereum/gather.sh
+++ benchmark/ethereum/gather.sh
@@ -13,8 +13,6 @@ for host in `cat $HOSTS`; do
     echo "adding peer to network, rpcport $RPCPORT"
     ssh $USER@$host chmod 755 $ETH_HOME/enode.sh
     echo "'admin.addPeer(` ssh $USER@$host $ETH_HOME/enode.sh $host 2>/dev/null | grep enode`)'" >> addPeer.txt
-    sleep 1
   fi
   let i=$i+1
-  echo $i
 done

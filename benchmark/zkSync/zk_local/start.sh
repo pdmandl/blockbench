1d86675b8f53718b91efcb2506c2979b55a498e5
paul.mandl <paul.mandl@findustrial.io>
2022-12-08 16:02:45 +0100

    b

--- benchmark/zkSync/zk_local/start.sh
+++ benchmark/zkSync/zk_local/start.sh
@@ -1,10 +1,9 @@
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
 
 cd /home/ubuntu/blockbench/benchmark/zkSync/zk_local
 sudo rm -rf ./volumes
 mkdir -p ./volumes
 mkdir -p ./volumes/postgres ./volumes/geth ./volumes/zksync/env/dev ./volumes/zksync/data
-
-docker-compose up
+chmod 755 ./volumes ./volumes/postgres ./volumes/geth ./volumes/zksync/env/dev ./volumes/zksync/data
+chown paulmandl ./volumes ./volumes/postgres ./volumes/geth ./volumes/zksync/env/dev ./volumes/zksync/data
+docker compose up
 

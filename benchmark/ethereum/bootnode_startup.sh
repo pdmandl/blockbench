1c21e4b65eb7dda71fc89e61b268b901c52b51e9
paul.mandl <paul.mandl@findustrial.io>
2022-12-08 17:58:07 +0100

    b

--- benchmark/ethereum/bootnode_startup.sh
+++ benchmark/ethereum/bootnode_startup.sh
@@ -3,4 +3,5 @@ cd `dirname ${BASH_SOURCE-$0}`
 . env.sh
 
 bootnode -genkey boot.key
+sudo kill -9 `sudo lsof -t -i:30310`
 bootnode -nodekey boot.key -verbosity 9 -addr :30310
--- benchmark/ethereum/setupPull.sh
+++ benchmark/ethereum/setupPull.sh
@@ -5,11 +5,11 @@ cd `dirname ${BASH_SOURCE-$0}`
 
 i=0
 for host in `cat $HOSTS`; do
-  ssh -oStrictHostKeyChecking=no $USER@$host "cd blockbench && git stash && git pull"
+  ssh -oStrictHostKeyChecking=no $USER@$host "cd blockbench && git add . && git stash && git pull"
   echo done node $host
 done
 for host in `cat $CLIENTS`; do
-  ssh -oStrictHostKeyChecking=no $USER@$host "cd blockbench && git stash && git pull"
+  ssh -oStrictHostKeyChecking=no $USER@$host "cd blockbench && git add . && git stash && git pull"
   echo done node $host
 done
 

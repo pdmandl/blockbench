5316a3b50757526f1361f81ed1bda6100326a4fc
paul.mandl <paul.mandl@findustrial.io>
2022-12-12 10:23:08 +0100

    bb

--- greeter-example/hardhat.config.ts
+++ greeter-example/hardhat.config.ts
@@ -1,8 +1,23 @@
-require("@matterlabs/hardhat-zksync-deploy");
-require("@matterlabs/hardhat-zksync-solc");
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
 
 module.exports = {
-  solidity: {
-    version: "0.8.16",
+  defaultNetwork: "matic",
+  networks: {
+    hardhat: {
+    },
+    polygon_mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: ["6ea44ded1928c25f5eb73e5240937379f451c7aaef49d32c065f78dc0cbf6da7"]
+    }
   },
-};
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+}

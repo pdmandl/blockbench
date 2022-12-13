bab917f9c9800a44462243da89c9947035c1b1ea
paul.mandl <paul.mandl@findustrial.io>
2022-12-13 23:32:51 +0100

    bb

--- benchmark/polygon/polyKvStore.js
+++ benchmark/polygon/polyKvStore.js
@@ -1,42 +1,25 @@
 const ethers = require("ethers");
 
-let url = "http://192.168.4.150:8545";
+let url = process.argv[3];
 let provider = new ethers.providers.JsonRpcProvider(url);
 var signer = new ethers.Wallet(process.argv[2], provider);
 
-var address = "0x0000000000000000000000000000000000001110";
+var address = "0x0000000000000000000000000000000000001111";
 var abi = [
   {
-    inputs: [
-      {
-        components: [
-          {
-            internalType: "string",
-            name: "name",
-            type: "string",
-          },
-        ],
-        internalType: "struct Greeter.MyStruct[]",
-        name: "myStr",
-        type: "tuple[]",
-      },
-      {
-        internalType: "int256[]",
-        name: "myArgNum",
-        type: "int256[]",
-      },
-      {
-        internalType: "string",
-        name: "mySecondStr",
-        type: "string",
-      },
-    ],
+    inputs: [],
     stateMutability: "nonpayable",
     type: "constructor",
   },
   {
-    inputs: [],
-    name: "greet",
+    inputs: [
+      {
+        internalType: "string",
+        name: "key",
+        type: "string",
+      },
+    ],
+    name: "get",
     outputs: [
       {
         internalType: "string",
@@ -48,51 +31,44 @@ var abi = [
     type: "function",
   },
   {
-    inputs: [],
-    name: "greetNum",
-    outputs: [
-      {
-        internalType: "int256",
-        name: "",
-        type: "int256",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "greetSecond",
-    outputs: [
+    inputs: [
       {
         internalType: "string",
-        name: "",
+        name: "key",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "value",
         type: "string",
       },
     ],
-    stateMutability: "view",
+    name: "set",
+    outputs: [],
+    stateMutability: "nonpayable",
     type: "function",
   },
 ];
-console.log(provider.getCode(address));
 myContract_write = new ethers.Contract(address, abi, signer); // Write only
 myContract_read = new ethers.Contract(address, abi, provider); // Read only
-
-// Writing to Smart Contract
-myContract_write
-  .greet()
-  .then((result) => {
-    console.log(result);
-  })
-  .catch((e) => {
-    console.log(e);
-  });
-// Reading from Smart Contract
-myContract_read
-  .greetNum()
-  .then((result) => {
-    console.log(result);
-  })
-  .catch((e) => {
-    console.log(e);
-  });
+const txs = Array.from(Array(process.argv[4]).keys());
+for (let i of txs) {
+  myContract_write
+    .set(i.toString(), "TEST 123")
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+}
+for (let i of txs) {
+  myContract_read
+    .get(i)
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+}
---
+++ greeter-example/contracts/Kvstore.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+contract KVstore {
+    mapping(string => string) store;
+
+    constructor() {}
+
+    function get(string memory key) public view returns (string memory) {
+        return store[key];
+    }
+
+    function set(string memory key, string memory value) public {
+        store[key] = value;
+    }
+}

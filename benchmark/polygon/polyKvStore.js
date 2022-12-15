5439120d01e75a5312c49f2de0ee1ea6f9c39d51
paul.mandl <paul.mandl@findustrial.io>
2022-12-15 23:26:00 +0100

    bb

--- benchmark/polygon/polyKvStore.js
+++ benchmark/polygon/polyKvStore.js
@@ -4,7 +4,7 @@ const doneTxs = [];
 let url = process.argv[3];
 let provider = new ethers.providers.JsonRpcProvider(url);
 var signer = new ethers.Wallet(process.argv[2], provider);
-
+var managedSigner = new NonceManager(signer);
 var address = "0x0000000000000000000000000000000000001111";
 var abi = [
   {
@@ -50,7 +50,7 @@ var abi = [
     type: "function",
   },
 ];
-const myContract_write = new ethers.Contract(address, abi, signer); // Write only
+const myContract_write = new ethers.Contract(address, abi, managedSigner); // Write only
 const myContract_read = new ethers.Contract(address, abi, provider); // Read only
 const savePacket = async (id, value) => {
   console.log("Saving Packet: " + value + " to id " + id + " started...");
@@ -82,7 +82,6 @@ const getGasPriceR = async (id) => {
     (await myContract_read.estimateGas.get(id)).toNumber()
   );
 };
-
 const readPacket = async (id) => {
   console.log("Reading id " + id + " started...");
   try {
@@ -107,8 +106,12 @@ const doRTransactions = async () => {
   }
 };
 const doWTransactions = async () => {
+  let result = [];
   const doneTxs = await Promise.all(txs);
-  for (let tx of doneTxs) console.log(tx);
+  for (let tx of doneTxs) {
+    result = [...result, tx];
+    console.log(tx);
+  }
   //doRTransactions();
 };
 const sleep = (ms) => new Promise((r) => setTimeout(r, ms));

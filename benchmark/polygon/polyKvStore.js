2ec6e67af21e3c642601b51e718ff15de1e36b44
paul.mandl <paul.mandl@findustrial.io>
2022-12-18 14:24:14 +0100

    b

--- benchmark/polygon/polyKvStore.js
+++ benchmark/polygon/polyKvStore.js
@@ -129,7 +129,7 @@ const measureTime = async () => {
     await sleep(1);
   }
   const end = Date.now();
-  console.log("the test took " + end - start + " to finish.");
+  console.log("the test took " + (end - start) + " to finish.");
 };
 const doTxs = async (txCount, run) => {
   await sleep(1000);

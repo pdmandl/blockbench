e94406ee00e935997414c1a235b26402d13e0e0f
paul.mandl <paul.mandl@findustrial.io>
2022-12-18 12:14:05 +0100

    b

--- benchmark/polygon/start.sh
+++ benchmark/polygon/start.sh
@@ -6,4 +6,4 @@ cd `dirname ${BASH_SOURCE-$0}`
 IP_ADDR_EXT=`(ip -4 addr | grep -oP '(?<=inet\s)\d+(\.\d+){3}') | cut -f2 -d$'\n'`
 sudo kill -9 `sudo lsof -t -i:1478`
 sudo kill -9 `sudo lsof -t -i:8545`
-polygon-edge server --data-dir $ETH_DATA --chain genesis.json  --libp2p 0.0.0.0:1478 --nat $IP_ADDR_EXT --seal > $ETH_DATA/../eth_log 2>&1 & 
+polygon-edge server --data-dir $ETH_DATA --max-enqueued 1024 --chain genesis.json  --libp2p 0.0.0.0:1478 --nat $IP_ADDR_EXT --seal > $ETH_DATA/../eth_log 2>&1 & 

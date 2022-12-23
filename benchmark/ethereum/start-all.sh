3a5597ec78f5c838602eb8fb6466d5d70af428ee
paul.mandl <paul.mandl@findustrial.io>
2022-12-23 19:11:08 +0100

    b

--- benchmark/ethereum/start-all.sh
+++ benchmark/ethereum/start-all.sh
@@ -12,10 +12,10 @@ for host in `cat $HOSTS`; do
   if [[ $i -lt $1 ]]; then
     echo start mining on node $host
     #scp -oStrictHostKeyChecking=no $ETH_HOME_LOCAL/addPeer.txt $USER@$host:$ETH_HOME
-    #scp -oStrictHostKeyChecking=no $ETH_HOME_LOCAL/bootnode.txt $USER@$host:$ETH_HOME
+    scp -oStrictHostKeyChecking=no $ETH_HOME_LOCAL/bootnode.txt $USER@$host:$ETH_HOME
     scp -oStrictHostKeyChecking=no $ETH_HOME_LOCAL/static-nodes.json $USER@$host:$ETH_DATA/geth
     #ssh -oStrictHostKeyChecking=no $USER@$host chmod 755 $ETH_HOME/addPeer.txt
-    #ssh -oStrictHostKeyChecking=no $USER@$host chmod 755 $ETH_HOME/bootnode.txt
+    ssh -oStrictHostKeyChecking=no $USER@$host chmod 755 $ETH_HOME/bootnode.txt
     ssh -oStrictHostKeyChecking=no $USER@$host chmod 755 $ETH_HOME/start-mining.sh
     ssh -oStrictHostKeyChecking=no $USER@$host chmod 755 $ETH_DATA/geth/static-nodes.json
     ssh -oStrictHostKeyChecking=no $USER@$host $ETH_HOME/start-mining.sh

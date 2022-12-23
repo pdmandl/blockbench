9734adbb41e97d393574cd5273775776d4ca1c98
pdmandl <p.d.mandl@gmail.com>
2022-12-23 22:54:00 +0100

    b

--- benchmark/ethereum/installClient.sh
+++ benchmark/ethereum/installClient.sh
@@ -1,8 +1,8 @@
 sudo apt autoremove
 sudo apt update
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-sudo apt install nodejs
-sudo apt install npm
+sudo aptitude install nodejs
+sudo aptitude install npm
 
 sudo npm install -g truffle
 sudo npm install -g ethers

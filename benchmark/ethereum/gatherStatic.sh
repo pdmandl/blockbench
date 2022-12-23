f48f1feb42cdc07e90037c837455d78a5541d787
paul.mandl <paul.mandl@findustrial.io>
2022-12-23 14:10:28 +0100

    bb

--- benchmark/ethereum/gatherStatic.sh
+++ benchmark/ethereum/gatherStatic.sh
@@ -4,19 +4,28 @@ cd `dirname ${BASH_SOURCE-$0}`
 . env.sh 
 
 i=0
+echo "starting gathering"
 echo "[" >> static-nodes.json
 for host in `cat $HOSTS`; do
   if [[ $i -lt 1 ]]; then
-    ssh $USER@$host chmod 755 $ETH_HOME/bootnode_startup.sh
-    echo -n `ssh $USER@$host $ETH_HOME/bootnode_startup.sh 2>/dev/null | grep enode` >> bootnode.txt
+    echo "starting gathering boot"
+    #ssh $USER@$host chmod 755 $ETH_HOME/bootnode_startup.sh
+    #echo -n `ssh $USER@$host $ETH_HOME/bootnode_startup.sh 2>/dev/null | grep enode` >> bootnode.txt
   fi
   if [[ $i -lt $1 ]]; then
     echo "adding peer to network, rpcport $RPCPORT"
     ssh $USER@$host chmod 755 $ETH_HOME/enode.sh
-    echo "`ssh $USER@$host $ETH_HOME/enode.sh $host 2>/dev/null | grep enode`," >> static-nodes.json
+    if [[ $i -lt `expr $1 - 1` ]]; then
+      echo "`ssh $USER@$host $ETH_HOME/enode.sh $host 2>/dev/null | grep enode`," >> static-nodes.json
+    else 
+      echo "`ssh $USER@$host $ETH_HOME/enode.sh $host 2>/dev/null | grep enode`" >> static-nodes.json
+    fi
     sleep 1
   fi
   let i=$i+1
   echo $i
 done
 echo "]" >> static-nodes.json
+for host in `cat $HOSTS`; do
+  if [[ $i -lt $1 ]]; then
+    ssh $USER@$host 

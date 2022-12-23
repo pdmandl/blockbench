8a532ab4a769a6b5e56e9d8e6fd1e06ad00d7b82
paul.mandl <paul.mandl@findustrial.io>
2022-12-23 14:10:32 +0100

    b

--- benchmark/ethereum/start-all.sh
+++ benchmark/ethereum/start-all.sh
@@ -11,12 +11,14 @@ sleep 3
 for host in `cat $HOSTS`; do
   if [[ $i -lt $1 ]]; then
     echo start mining on node $host
-    scp -oStrictHostKeyChecking=no $ETH_HOME_LOCAL/addPeer.txt $USER@$host:$ETH_HOME
-    scp -oStrictHostKeyChecking=no $ETH_HOME_LOCAL/bootnode.txt $USER@$host:$ETH_HOME
-    ssh -oStrictHostKeyChecking=no $USER@$host chmod 755 $ETH_HOME/addPeer.txt
-    ssh -oStrictHostKeyChecking=no $USER@$host chmod 755 $ETH_HOME/bootnode.txt
+    #scp -oStrictHostKeyChecking=no $ETH_HOME_LOCAL/addPeer.txt $USER@$host:$ETH_HOME
+    #scp -oStrictHostKeyChecking=no $ETH_HOME_LOCAL/bootnode.txt $USER@$host:$ETH_HOME
+    scp -oStrictHostKeyChecking=no $ETH_HOME_LOCAL/static-nodes.json $USER@$host:$ETH_DATA/geth
+    #ssh -oStrictHostKeyChecking=no $USER@$host chmod 755 $ETH_HOME/addPeer.txt
+    #ssh -oStrictHostKeyChecking=no $USER@$host chmod 755 $ETH_HOME/bootnode.txt
     ssh -oStrictHostKeyChecking=no $USER@$host chmod 755 $ETH_HOME/start-mining.sh
-    ssh -oStrictHostKeyChecking=no $USER@$host $ETH_HOME/start-mining.sh $PEERS $BOOTNODE
+    ssh -oStrictHostKeyChecking=no $USER@$host chmod 755 $ETH_DATA/static-nodes.json
+    ssh -oStrictHostKeyChecking=no $USER@$host $ETH_HOME/start-mining.sh
     echo done node $host
   fi
   let i=$i+1

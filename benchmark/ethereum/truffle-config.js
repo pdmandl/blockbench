120cc819a60591d56d056af0c335eccab1aeee0c
pdmandl <p.d.mandl@gmail.com>
2022-12-27 12:06:58 +0100

    b

--- benchmark/ethereum/truffle-config.js
+++ benchmark/ethereum/truffle-config.js
@@ -21,7 +21,7 @@ const HDWalletProvider = require("@truffle/hdwallet-provider");
 //
 // const fs = require('fs');
 const mnemonic =
-  "0x7726827caac94a7f9e1b160f7ea819f172f7b6f9d2a97f992c38edeab82d4110";
+  "7726827caac94a7f9e1b160f7ea819f172f7b6f9d2a97f992c38edeab82d4110";
 
 module.exports = {
   /**

4ecdb7f19a23b525e6eaebc3a2329b991023875f
pdmandl <p.d.mandl@gmail.com>
2022-12-27 09:39:24 +0100

    b

--- benchmark/ethereum/installClient.sh
+++ benchmark/ethereum/installClient.sh
@@ -1,4 +1,4 @@
-sudo apt autoremove
+echo Installation startet at $(date -u)
 sudo apt-mark unhold nodejs
 sudo apt-mark unhold npm
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
@@ -9,4 +9,4 @@ sudo npm install -g truffle
 sudo npm install -g ethers
 sudo npm install -g @ethersproject/experimental
 sudo npm install -g @truffle/hdwallet-provider
-
+echo Installation finished at $(date -u)

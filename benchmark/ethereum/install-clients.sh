67795b8ebf9d31e459c4d01a874fb06d386cd4b0
pdmandl <p.d.mandl@gmail.com>
2022-12-27 09:55:57 +0100

    b

--- benchmark/ethereum/install-clients.sh
+++ benchmark/ethereum/install-clients.sh
@@ -6,8 +6,8 @@ cd `dirname ${BASH_SOURCE-$0}`
 i=0
 for host in `cat $CLIENTS`; do
   if [[ $i -lt $1 ]]; then
-    #ssh -oStrictHostKeyChecking=no $USER@$host chmod 755 $ETH_HOME/installClient.sh 
-    #ssh -oStrictHostKeyChecking=no $USER@$host $ETH_HOME/installClient.sh
+    ssh -oStrictHostKeyChecking=no $USER@$host chmod 755 $ETH_HOME/installClient.sh 
+    ssh -oStrictHostKeyChecking=no $USER@$host $ETH_HOME/installClient.sh
     echo done client $i at $host
   fi
   let i=$i+1
@@ -17,7 +17,7 @@ for host in `cat $HOSTS`; do
   if [[ $i -lt $1 ]]; then
     ssh -oStrictHostKeyChecking=no $USER@$host chmod 755 $ETH_HOME/installClient.sh 
     ssh -oStrictHostKeyChecking=no $USER@$host $ETH_HOME/installClient.sh
-    echo done client $i at $host
+    echo done host $i at $host
   fi
   let i=$i+1
 done
--- benchmark/ethereum/installClient.sh
+++ benchmark/ethereum/installClient.sh
@@ -1,9 +1,9 @@
 echo Installation startet at $(date -u)
-sudo apt-mark unhold nodejs
-sudo apt-mark unhold npm
-curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-sudo apt install nodejs
-sudo apt install npm
+#sudo apt-mark unhold nodejs
+#sudo apt-mark unhold npm
+#curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+#sudo apt install nodejs
+#sudo apt install npm
 
 sudo npm install -g truffle
 sudo npm install -g ethers

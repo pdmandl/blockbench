289706ca5394aade86283c8bcc2133b94568407e
pdmandl <p.d.mandl@gmail.com>
2022-12-28 13:56:01 +0100

    loging contract address

--- benchmark/ethereum/migrations/2_deploy_contract.js
+++ benchmark/ethereum/migrations/2_deploy_contract.js
@@ -3,6 +3,8 @@ var Smallbank = artifacts.require("Smallbank");
 
 module.exports = function (deployer) {
   // deployment steps
-  deployer.deploy(Kvstore);
-  deployer.deploy(Smallbank);
+  deployer.deploy(Kvstore).then(() => console.log(Kvstore.address));
+  deployer
+    .deploy(Smallbank, { gas: 5000000 })
+    .then(() => console.log(Smallbank.address));
 };

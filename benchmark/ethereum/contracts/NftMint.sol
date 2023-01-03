662960553528e4b9689edbcdf20b1423840dead3
pdmandl <p.d.mandl@gmail.com>
2023-01-03 02:20:07 +0100

    nb

--- benchmark/ethereum/contracts/NftMint.sol
+++ benchmark/ethereum/contracts/NftMint.sol
@@ -2,9 +2,9 @@
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts@4.3.0/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts@4.3.0/access/Ownable.sol";
-import "@openzeppelin/contracts@4.3.0/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
 contract NftMint is ERC721, ERC721Enumerable, Ownable {
     bool public saleIsActive = true;

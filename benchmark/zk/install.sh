e3ec3e388955319a3bdeafa4be78fe1075dbf32b
pdmandl <p.d.mandl@gmail.com>
2023-01-04 00:28:34 +0100

    b

--- benchmark/zk/install.sh
+++ benchmark/zk/install.sh
@@ -5,9 +5,7 @@
 #sudo add-apt-repository -y ppa:ethereum/ethereum-dev
 #sudo apt-get install -y apt-transport-https ca-certificates
 #sudo apt install docker-compose
-sudo usermod -aG docker ubuntu
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-sudo service docker restart
+sudo apt-get install docker-compose-plugin
 #sudo snap install docker
 #sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 #echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" | sudo tee /etc/apt/sources.list.d/docker.list

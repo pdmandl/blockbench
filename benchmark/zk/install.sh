528c1517c837a248847e96dc290a945754683f40
pdmandl <p.d.mandl@gmail.com>
2023-01-04 00:30:27 +0100

    b

--- benchmark/zk/install.sh
+++ benchmark/zk/install.sh
@@ -5,8 +5,9 @@
 #sudo add-apt-repository -y ppa:ethereum/ethereum-dev
 #sudo apt-get install -y apt-transport-https ca-certificates
 #sudo apt install docker-compose
-sudo apt-get install docker-compose-plugin
-#sudo snap install docker
+DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+mkdir -p $DOCKER_CONFIG/cli-plugins
+curl -SL https://github.com/docker/compose/releases/download/v2.14.2/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose#sudo snap install docker
 #sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 #echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" | sudo tee /etc/apt/sources.list.d/docker.list
 #sudo apt-get update

472e4eec61285722efc3806b9d2d780774d96a3b
pdmandl <p.d.mandl@gmail.com>
2023-01-03 11:27:49 +0100

    b

--- benchmark/ethereum/ethNftMint.js
+++ benchmark/ethereum/ethNftMint.js
@@ -563,7 +563,7 @@ const mintNft = async (numberOfItems, id) => {
   const start = Date.now();
   try {
     const res = await myContract_write.mint(numberOfItems, {
-      value: ethers.utils.parseUnits(0.2 * numberOfItems, "ether"),
+      value: ethers.utils.parseUnits((0.2 * numberOfItems).toString(), "ether"),
       gasLimit: 5000000,
     });
     const receipt = await res.wait();

3e8479c80e74dc3455027c1ac065c9d7d4b5bfe2
pdmandl <p.d.mandl@gmail.com>
2023-01-04 14:46:51 +0100

    b

--- benchmark/zk/scripts/deploy.js
+++ benchmark/zk/scripts/deploy.js
@@ -13,6 +13,7 @@ const { Deployer } = require("@matterlabs/hardhat-zksync-deploy");
 async function main(deployer) {
   const artifact = await deployer.loadArtifact("Smallbank");
   const res = await deployer.deploy(artifact);
+  console.log(res);
   const resp = await res.wait();
   console.log(resp);
 }

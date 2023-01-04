4e63521c1076f6ebd29aa5a1d2463fe3ac6fc784
pdmandl <p.d.mandl@gmail.com>
2023-01-04 12:46:12 +0100

    b

--- benchmark/zk/contracts/Kvstore.sol
+++ benchmark/zk/contracts/Kvstore.sol
@@ -1,13 +1,16 @@
-pragma solidity ^0.4.0;
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
 
-contract KVstore {
+contract Kvstore {
+    mapping(string => string) store;
 
-  mapping(string=>string) store;
+    constructor() {}
 
-  function get(string key) constant returns(string) {
-    return store[key];
-  }
-  function set(string key, string value) {
-    store[key] = value;
-  }
+    function get(string memory key) public view returns (string memory) {
+        return store[key];
+    }
+
+    function set(string memory key, string memory value) public {
+        store[key] = value;
+    }
 }

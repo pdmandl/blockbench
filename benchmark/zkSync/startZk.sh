46fde95cfa2e9633c41a0970a29c10c8c64bc135
pdmandl <p.d.mandl@gmail.com>
2023-01-04 01:32:01 +0100

    b

--- benchmark/zkSync/startZk.sh
+++ benchmark/zkSync/startZk.sh
@@ -9,5 +9,4 @@ mkdir -p ./volumes/postgres ./volumes/geth ./volumes/zksync/env/dev ./volumes/zk
 sudo chmod 755 ./volumes ./volumes/postgres ./volumes/geth ./volumes/zksync/env/dev ./volumes/zksync/data
 sudo chown ubuntu:ubuntu ./volumes ./volumes/postgres ./volumes/geth ./volumes/zksync/env/dev ./volumes/zksync/data
 sudo kill -9 $(sudo lsof -t -i:8555)
-docker-compose up
 

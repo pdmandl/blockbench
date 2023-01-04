98db6dc63b71dedab33d69ebbb554a64fca10e6b
pdmandl <p.d.mandl@gmail.com>
2023-01-04 13:17:09 +0100

    b

--- benchmark/zk/scripts/deploy.js
+++ benchmark/zk/scripts/deploy.js
@@ -4,14 +4,15 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
-console.log(process.argv[5]);
+const RICH_WALLET_PK =
+  "0x7726827caac94a7f9e1b160f7ea819f172f7b6f9d2a97f992c38edeab82d4110";
 const hre = require("hardhat");
 const { Wallet, Provider, Contract } = require("zksync-web3");
 const { Deployer } = require("@matterlabs/hardhat-zksync-deploy");
 
 async function main(deployer) {
-  const artifact = await deployer.loadArtifact("Greeter");
-  const res = await deployer.deploy(artifact, ["Hi"]);
+  const artifact = await deployer.loadArtifact("Smallbank");
+  const res = await deployer.deploy(artifact);
 }
 const provider = Provider.getDefaultProvider();
 const wallet = new Wallet(RICH_WALLET_PK, provider);
